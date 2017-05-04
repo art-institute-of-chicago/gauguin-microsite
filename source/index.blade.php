edf8bb136ceecfccdd0767bdf112e9b8fc8b97e8
Illya Moskvin <imoskvin@artic.edu>
2017-05-04 13:00:27 -0500

    Add rough header styles

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,22 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+
+<!-- Sticky Header -->
+<header>
+	<a id="aic-link" href="http://www.artic.edu/">
+		<img id="aic-logo" src="images/logo.svg">
+	</a>
+	<span class="exhibit">
+		<span class="title">Gauguin</span>
+		<span class="pipe">|</span>
+		<span class="subtitle">Artist as Alchemist</span>
+	</span>
+	<span class="buttons">
+		<span class="dates">June 25 – Sept 10</span>
+		<a class="btn btn-small" href="#" >Become a Member</a>
+		<a class="btn btn-small" href="#" >Get Tickets</a>
+	</span>
+</header>
+
 @endsection
