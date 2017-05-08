535bd557cf74be6dfc4a80f120a44d049ba0765d
nikhiltri <me@nikhiltrivedi.com>
2017-05-08 15:00:57 -0500

    Add footer to layout

---
+++ source/_partials/footer.blade.php
@@ -0,0 +1,16 @@
+<footer>
+	<div class="footer-left">
+
+	</div>
+
+	<div class="footer-right">
+
+		<span class="buttons">
+			<a href="http://www.facebook.com/artic"><img src="images/social_icons/facebook-128.png" alt="Facebook"></a>
+			<a href="https://twitter.com/artinstitutechi"><img src="images/social_icons/twitter-128.png" alt="Twitter"></a>
+			<a href="http://www.youtube.com/user/ArtInstituteChicago"><img src="images/social_icons/youtube-128.png" alt="YouTube"></a>
+			<a href="https://www.pinterest.com/artinstitutechi/"><img src="images/social_icons/pinterest-128.png" alt="Pinterest"></a>
+		</span>
+
+	</div>
+</footer>
