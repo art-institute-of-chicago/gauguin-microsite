c28ddeb81a40932576d92891032d2ce0e38ee17a
Illya Moskvin <imoskvin@artic.edu>
2017-05-09 18:24:06 -0500

    Add theme with one image in portrait layout

---
+++ source/_partials/theme/1_portrait.blade.php
@@ -0,0 +1,18 @@
+<section class="theme theme-1-portrait">
+
+	<h2>{{ $page->theme }}</h2>
+
+	<div class="wrapper">
+
+		<img src="images/content/{{ $page->image }}">
+
+		<div class="content">
+
+			<h1>{{ $page->title }}</h1>
+			<div>{!! $content !!}<div>
+
+		</div>
+
+	</div>
+
+</section>
