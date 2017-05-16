9fa9725f86976ab288228fdcb7ba611bfd39a7fc
Illya Moskvin <imoskvin@artic.edu>
2017-05-16 10:39:13 -0500

    Remove BrowserSync from Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var elixir = require('laravel-elixir');
 var argv = require('yargs').argv;
 var bin = require('./tasks/bin');
 
+var httpServer = require('http-server');
+var open = require('open');
+
 require('laravel-elixir-remove');
 
 elixir.config.assetsPath = 'source/_assets';
@@ -12,37 +15,57 @@ elixir.config.publicPath = 'source';
 elixir.config.sourcemaps = false;
 
 elixir(function(mix) {
+
     var env = argv.e || argv.env || 'local';
     var port = argv.p || argv.port || 3000;
 
-    mix.sass('main.scss')
-        .sass('reset.scss')
-        .scripts(['main.js'], './source/js/main.js')
-        .remove('source/**/*~')
-        .exec(bin.path() + ' build ' + env, [
-            './source/*',
-            './source/**/*',
-            '!./source/_assets/**/*'
-        ])
+    // @imports all the other SCSS partials
+    mix.sass('main.scss');
 
-        // TODO: Run synchronously or put this logic into the upload script.
-        // This removes Jigsaws direct-access to blocks as pages.
-        // .remove('build_' + env + '/blocks/**')
+    // Standardize baseline styles across browsers
+    mix.sass('reset.scss');
 
-        .browserSync({
-            port: port,
-            server: { baseDir: 'build_' + env },
-            proxy: null,
-            files: [ 'build_' + env + '/index.html' ],
+    // Combine all scripts into main.js
+    // Jigsaw will take care of copying it to build_*
+    mix.scripts(
+        [
+            'header.js',
+            'lightbox.js',
+            'slideshow.js',
+        ],
+        './source/js/main.js'
+    );
 
-            // https://github.com/BrowserSync/browser-sync/issues/344
-            notify: false,
-            open: false,
+    // Remove any emacs backup files
 
-            // https://github.com/tightenco/jigsaw/issues/86
-            // reloadDebounce: 500,
-            // reloadOnRestart: true,
+    mix.remove('source/**/*~');
 
-        });
+    // Run Jigsaw
+    mix.exec(bin.path() + ' build ' + env, [
+        './source/*',
+        './source/**/*',
+        '!./source/_assets/**/*'
+    ]);
+
+    // Run a simple HTTP server on `port`
+    var server = httpServer.createServer({
+        root: './build_' + env
+    });
+
+    server.listen(port);
+
+    // Open the site in browser
+    open('http://localhost:' + port);
+
+
+    // TODO: Add LiveReload?
+    // https://github.com/napcs/node-livereload
+
+    // For now, go download it manually:
+    // http://livereload.com/
+
+    // Regardless, you'll need the Chrome extension:
+    // https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
+
 });
 
