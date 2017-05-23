d00613ab65808c2c17f84c17dd395086532642a9
nikhiltri <me@nikhiltrivedi.com>
2017-05-23 17:41:33 -0500

    Update footer to sponsors block

---
+++ source/_partials/sponsors.blade.php
@@ -0,0 +1,59 @@
+<section class="sponsors">
+	<div class="container">
+
+		<div class="links">
+
+			<span class="buttons">
+
+				<a href="http://www.facebook.com/artic">
+					<img src="images/social_icons/facebook.png" alt="Facebook">
+				</a>
+
+				<a href="https://twitter.com/artinstitutechi">
+					<img src="images/social_icons/twitter.png" alt="Twitter">
+				</a>
+
+				<a href="https://www.instagram.com/artinstitutechi">
+					<img src="images/social_icons/instagram.png" alt="Instagram">
+				</a>
+
+				<a href="http://www.youtube.com/user/ArtInstituteChicago">
+					<img src="images/social_icons/youtube.png" alt="YouTube">
+				</a>
+
+			</span>
+
+        </div>
+
+		<div class="organized">
+
+            <h5>The exhibition is organized by</h5>
+
+            <div>
+                <img class="aic" src="images/logo.svg" alt="Art Institute of Chicago" />
+                <img src="images/sponsors/white_reunion-des-musees-nationaux_053483_reunion-des-musees-nationaux.png" alt="Réunion des Musées Nationaux" />
+                <img src="images/sponsors/white_musée_d'Orsay-2.png" alt="Musée d'Orsay" />
+            </div>
+
+        </div>
+
+        <div class="sponsor">
+
+            <h5>Lead Sponsor</h5>
+
+            <div>
+                <img src="images/sponsors/bom_white.png" alt="Bank of America" />
+            </div>
+
+        </div>
+
+        <div class="support">
+
+            <h5>Major support is provided by</h5>
+
+            <h3>Lesley and Janice Lederer</h3>
+
+        </div>
+
+	</div>
+</section>
