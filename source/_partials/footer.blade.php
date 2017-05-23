8f345346bd924b3fe451e9bb5352dd4f95b3d8ca
nikhiltri <me@nikhiltrivedi.com>
2017-05-23 17:41:57 -0500

    Add second footer block

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,33 +1,10 @@
 <footer>
-	<div class="container">
+  <div class="text">
+	{!! $content !!}
+  </div>
 
-		<div class="footer-left">
-
-		</div>
-
-		<div class="footer-right">
-
-			<span class="buttons">
-
-				<a href="http://www.facebook.com/artic">
-					<img src="images/social_icons/facebook-128.png" alt="Facebook">
-				</a>
-
-				<a href="https://twitter.com/artinstitutechi">
-					<img src="images/social_icons/twitter-128.png" alt="Twitter">
-				</a>
-
-				<a href="http://www.youtube.com/user/ArtInstituteChicago">
-					<img src="images/social_icons/youtube-128.png" alt="YouTube">
-				</a>
-
-				<a href="https://www.pinterest.com/artinstitutechi/">
-					<img src="images/social_icons/pinterest-128.png" alt="Pinterest">
-				</a>
-
-			</span>
-
-		</div>
-
-	</div>
+  <div class="sponsor">
+    Digital engagement support by<br/>
+    <img src="images/sponsors/bloomberg.png" alt="Bloomberg Philanthropies" />
+  </div>
 </footer>
